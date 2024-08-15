26615ff04deb0faa1b840c2818915bf180feced3
JUANJO <86635450+jjchaparro@users.noreply.github.com>
2024-08-15 15:13:21 -0400

    Creando el proyecto para ejercicios

---
+++ app.js
@@ -0,0 +1,47 @@
+let numeroUn = 1;
+
+let stringUn = '1';
+
+let numeroTreinta = 30;
+
+let stringTreinta = '30';
+
+let numeroDiez = 10;
+
+let stringDiez = '10';
+
+
+
+if (1 == '1' || 1 === '1') {
+
+    console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
+  
+  } else {
+  
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+  
+  };
+  
+   
+  
+  if (30 === '30') {
+  
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
+  
+  } else {
+  
+    console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
+  
+  };
+  
+   
+  
+  if (10 == '10' || 10 === '10') {
+  
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
+  
+  } else {
+  
+    console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
+  
+  };
